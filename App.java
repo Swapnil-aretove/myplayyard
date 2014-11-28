26ba8d5c5c4075ce91d835131819be8ca604030f
Swapnil Somwanshi <swapnil.somwanshi@aretove.com>
2014-11-28 12:54:17 +0530

    Just a small formating

--- App.java
+++ App.java
@@ -1,8 +1,8 @@
 public class App{
 
-public static void main(String [] args)
-{
-System.out.println("This is git testing");
-}
+	public static void main(String [] args)
+	{
+		System.out.println("This is git testing");
+	}
 
 }
